0500447bc6e173b2b1f11f30f1485098440845a2
huyk <bengda@outlook.com>
2020-03-23 17:13:04 +0800

    update:vue-hu-dialog文档

---
+++ vue-h-dialog/js/chunk-2d21d6e3.65499b2e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21d6e3"],{d0ef:function(e,n,d){"use strict";d.r(n),n["default"]="```js\r\nimport vueDialog from 'vue-h-dialog';\r\n\r\nconst gId = vueDialog().gId;\r\n\r\nvueDialog.close(gId);\r\n\r\n// {{ i18n('关闭所有') }}\r\nvueDialog.close.apply(null, [...document.querySelectorAll('[data-vhd-gid]')].map((el) => el.dataset.vhdGid));\r\n```"}}]);
+//# sourceMappingURL=chunk-2d21d6e3.65499b2e.js.map
